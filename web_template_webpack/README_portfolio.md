b8d0ffccde98d39cf3b26c9d6412200366b81974
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-08-18 14:58:02 +0700

    add webpack web template

---
+++ web_template_webpack/README_portfolio.md
@@ -0,0 +1,31 @@
+# Team Treehouse Tech Degree - ___ - P___ - ___
+
+## Introduction
+
+This is the ___ project of ___ . The complete instruction of the project can be seen in [project_instructions.pdf](https://github.com/wahidyankf/___/blob/master/project_instructions.pdf), and the initial code and assets can be seen in the `initial_assets` folder.
+
+## Usage
+
+### Demo pages
+
+You can see the demo page [here](https://wahidyankf.github.io/___/).
+
+### Result
+
+The result can be seen in the gh-pages branch of this repository.
+
+### The Source
+
+The source of this app can be seen in the src folder of master branch. 
+
+## Testing
+
+This app was tested using [HTML](https://validator.w3.org/) and [CSS](https://jigsaw.w3.org/css-validator/) validator.
+
+## Sreenshots
+
+![screenshot 1](screenshots/___.jpg)
+
+## License
+
+[MIT](https://en.wikipedia.org/wiki/MIT_License).
---
+++ web_template_webpack/webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = ['jquery'];
+
+const config = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        }),
+        test: /\.scss$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
+
+module.exports = config;
