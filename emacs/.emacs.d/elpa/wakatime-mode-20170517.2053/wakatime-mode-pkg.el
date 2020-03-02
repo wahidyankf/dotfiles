49b9e847a004a7c0e0f07c0b39d43a23b6cc5649
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2020-03-02 20:44:44 +0700

    auto commit

--- emacs/.emacs.d/elpa/wakatime-mode-20170517.2053/wakatime-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "wakatime-mode" "20170517.2053" "Automatic time tracking extension for WakaTime" 'nil)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,35 +1,47 @@
-(require 'package)
-(tool-bar-mode -1)
-
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(setq package-enable-at-startup nil)
-(package-initialize)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (wakatime-mode helm evil-visual-mark-mode))))
+ '(line-number-mode nil)
+ '(org-agenda-files
+   (quote
+    ("~/wkf-org/wkf.org" "~/wkf-org/ruangguru/ruangguru.org")))
+ '(package-selected-packages
+   (quote
+    (wakatime-mode ob-ipython ob-typescript elixir-mode ob-elixir use-package try projectile htmlize helm gruvbox-theme flycheck evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Dank Mono" :foundry "nil" :slant normal :weight normal :height 110 :width normal)))))
 
-;; -- evil mode --
-(require 'evil)
-(evil-mode t)
+;; --- our file
 
-;; -- helm mode --
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
+;; Setup package manager
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
-;; -- wakatime mode --
-;; (global-wakatime-mode)
+(package-initialize)
 
+;; Package - Use Package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+;; install all packages we need
+(dolist (package '(evil helm projectile htmlize gruvbox-theme elixir-mode wakatime-mode ob-typescript ob-elixir ob-ipython))
+  (unless (package-installed-p package)
+   (package-install package))
+   (require package))
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "config.org" user-emacs-directory))
