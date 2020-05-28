ec018e6c98e7b46f9e668cf54a9d10a4da0da2a3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2020-05-28 09:45:46 +0700

    auto commit

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -3,28 +3,9 @@
 
 (setq doom-theme 'doom-one)
 
-;; If you use`org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/wkf-org/")
 
-    ;; This determines the style of line numbers in effect.If set to`nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
-
-;; Here are some additional functions / macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-    ;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-    ;; this file.Emacs searches the`load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
 
 (use-package! wakatime-mode
   :hook (after-init . global-wakatime-mode))
@@ -60,6 +41,12 @@
          lsp-ui-doc-max-width 50
          lsp-ui-sideline-ignore-duplicate t))
 
+(use-package! company-lsp
+  :after lsp-mode
+  :config
+  (set-company-backend! 'lsp-mode 'company-lsp)
+  (setq company-lsp-enable-recompletion t))
+
 (use-package! dap-mode
   :after lsp-mode
   :config
@@ -73,12 +60,6 @@
                    (when (equal major-mode 'reason-mode)
                      (refmt)))))
 
-(use-package! company-lsp
-  :after lsp-mode
-  :config
-  (set-company-backend! 'lsp-mode 'company-lsp)
-  (setq company-lsp-enable-recompletion t))
-
 (use-package! lsp-typescript
   :when (featurep! +javascript)
   :hook ((js2-mode typescript-mode) . lsp-typescript-enable))
@@ -86,7 +67,6 @@
 (use-package! lsp-css
   :when (featurep! +css)
   :hook ((css-mode less-mode scss-mode) . lsp-css-enable))
-
 
 (when (featurep! +sh)
   (after! sh-script
