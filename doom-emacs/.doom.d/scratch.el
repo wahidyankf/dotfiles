6c524beb2aad3265a6ec210a438471431e0d5c29
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2020-05-31 10:18:34 +0700

    auto commit

--- doom-emacs/.doom.d/scratch.el
+++ doom-emacs/.doom.d/scratch.el
@@ -1,18 +1,1 @@
-;;; ~/Dropbox/wkf-devbox/doom-emacs/.doom.d/scratch.el -*- lexical-binding: t; -*-
-
-(defun wkf/save-buffer ()
-  "Save current buffer with custom lsp formatting"
-  (interactive)
-  (cond ((bound-and-true-p lsp-mode)
-         (cond ((equal major-mode 'reason-mode)
-                (lsp-format-buffer))
-               (t (progn (lsp-format-buffer)))))
-        ((equal major-mode 'emacs-lisp-mode)
-         (progn (elisp-format-buffer)))
-        (t nil))
-  (save-buffer))
-
-(define-key evil-normal-state-map (kbd ", w") 'wkf/save-buffer)
-(define-key evil-normal-state-map (kbd ", q") 'delete-window)
-
-(delete-window)
+(setq auto-revert-mode t)
